1f77aed1e8b68314b8ff841d177672bf841c7e55
Ousmato <ousmatotoure98@gmail.com>
2024-01-09 17:51:37 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     slidesPerView: 2, // Affiche deux slides à la fois
+//     spaceBetween: 30, // Espacement entre les slides
+
+//     direction: 'horizontal', //
+   
+  
+//     // If we need pagination
+//     pagination: {
+    
+//       el: '.swiper-pagination',
+//     },
+    
+// });
